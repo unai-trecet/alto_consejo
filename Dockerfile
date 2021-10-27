3e5db4c34a44fce0d02af906e2660ba85911eba2
unai <unai13t@gmail.com>
2021-10-27 16:34:24 +0200

    Use mailcatcher; Use postgresql instead of sqlite; Tweak Docker files.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM ruby:3.0.2
 RUN apt-get update -qq \ 
-&& apt-get install -y nodejs sqlite3 libsqlite3-dev yarn \
+&& apt-get install -y nodejs postgresql-client yarn \
 && apt-get clean
 
 ADD . /alto_consejo
 WORKDIR /alto_consejo
 RUN bundle install
 
+COPY entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
 
 CMD ["bash"]
+
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,17 +76,13 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.action_mailer.delivery_method = :smtp
+
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    # user_name: ENV['email'],
-    user_name: 'altoconsehofriki@gmail.com',
-    # password: ENV['password'],
-    password: 'Andorra_200:',
-    domain: ENV['localhost:3000'],
-    address: 'smtp.gmail.com',
-    port: '587',
-    authentication: :plain,
-    enable_starttls_auto: true
+  config.action_mailer.smtp_settings = { 
+    address: 'mailcatcher',
+    port: 1025 
   }
 end
