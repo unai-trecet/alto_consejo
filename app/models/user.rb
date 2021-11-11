4c0423603756b695ba2b3da7ea63b3b134814bd4
unai <unai13t@gmail.com>
2021-11-11 11:42:44 +0100

    Eliminate self created auth system.

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Email inválido.' }
-  validates :password, presence: true, length: { minimum: 8 }
-end
