705c8d9147790899c28632ec18785de6fd1e5e8e
unai <unai13t@gmail.com>
2021-11-24 18:03:56 +0100

    Add few game model tests.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 end
