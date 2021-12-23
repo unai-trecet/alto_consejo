58511ae4ab1d7508e22c627090dafbc350b504a0
unai <unai13t@gmail.com>
2021-12-23 11:03:11 +0100

    Add missing models specs.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:game) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:start_at) }
+  it { should validate_presence_of(:end_at) }
 end
