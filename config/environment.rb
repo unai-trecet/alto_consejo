f2f5b8d1eb39eb04da229084ffbce5c1e1f9204d
unai <unai13t@gmail.com>
2021-12-27 18:21:02 +0100

    Set default_url_options.

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set the default host and port to be the same as Action Mailer.
+AltoConsejo::Application.default_url_options = AltoConsejo::Application.config.action_mailer.default_url_options
