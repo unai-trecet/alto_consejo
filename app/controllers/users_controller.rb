d60dcdd45d3d583ed9c9affaff1f82850de29b85
unai <unai13t@gmail.com>
2021-12-28 19:45:44 +0100

    Add edit and update actions and views for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
+  before_action :require_permission, except: %i[show index]
 
   def index
     @users = if params[:q]
@@ -18,9 +19,33 @@ class UsersController < ApplicationController
 
   def show; end
 
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: t('user_changed') }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_permission
+    return if current_user == @user
+
+    redirect_to root_path, flash: { error: t('custom_errors.unauthorized') }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let!(:rodolfo) { create(:user, :confirmed, username: 'rodolfo') }
+  let(:valid_params) do
+    {
+      email: 'rodolfo@test.com',
+      username: 'rodolfo',
+      password: '12345678',
+      password_confirmation: '12345678'
+    }
+  end
+
+  let(:invalid_params) { valid_params.merge({ username: nil }) }
+
+  let!(:rodolfo) { create(:user, :confirmed, valid_params) }
+
+  def set_params(input = {})
+    valid_params.merge(input)
+  end
 
   describe 'GET /index' do
     let!(:users) { create_list(:user, 4, :confirmed) }
@@ -68,6 +83,105 @@ RSpec.describe 'Users', type: :request do
 
         expect(response).to be_successful
         expect(response).to render_template(:show)
+      end
+    end
+  end
+
+  describe 'GET /edit' do
+    def call_action(user = rodolfo)
+      get edit_user_url(user)
+    end
+
+    it_behaves_like 'not_logged_in'
+
+    context 'user authenticated' do
+      before { sign_in(rodolfo) }
+
+      it 'render a successful response' do
+        call_action
+
+        expect(response).to be_successful
+        expect(response).to render_template(:edit)
+      end
+
+      it 'redirects to root if user is not current user' do
+        user = create(:user, :confirmed)
+
+        call_action(user)
+
+        expect(response).to redirect_to(root_path)
+        expect(flash[:error]).to eq('No estás autorizado a realizar esa acción.')
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    let(:user) do
+      create(:user, :confirmed,
+             set_params({ username: 'old_username', email: 'other@email.com' }))
+    end
+
+    def call_action(params = valid_params)
+      patch user_url(user), params: { user: params }
+    end
+
+    it_behaves_like 'not_logged_in'
+
+    context 'user authenticated' do
+      before { sign_in(user) }
+
+      context 'with valid parameters' do
+        it 'updates the requested user username' do
+          expect(user.username).to eq('old_username')
+
+          new_params = set_params({ username: 'new_username' })
+          call_action(new_params)
+
+          user.reload
+          expect(user.username).to eq('new_username')
+          expect(response).to redirect_to(user_url(user))
+        end
+
+        it 'does not update any other attribute but username' do
+          expect(user.username).to eq('old_username')
+          expect(user.valid_password?('12345678')).to eq(true)
+
+          new_pass = '87654321'
+          new_params = { username: 'new_username',
+                         email: 'another@email.com',
+                         password: new_pass,
+                         password_confirmation: new_pass }
+
+          call_action(new_params)
+
+          user.reload
+          expect(user.valid_password?(new_pass)).to eq(false)
+          expect(user.username).to eq('new_username')
+          expect(user.email).to eq('other@email.com')
+        end
+
+        it 'does not update user if user is not the logged in user' do
+          sign_out(user)
+          sign_in(rodolfo)
+          expect(user.username).to eq('old_username')
+
+          new_params = set_params({ username: 'new_username' })
+          call_action(new_params)
+
+          user.reload
+          expect(user.username).to eq('old_username')
+          expect(response).to redirect_to(root_path)
+          expect(flash[:error]).to eq('No estás autorizado a realizar esa acción.')
+        end
+      end
+
+      context 'with invalid parameters' do
+        it "renders a successful response (i.e. to display the 'edit' template)" do
+          call_action(invalid_params)
+
+          expect(response).to render_template(:edit)
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
