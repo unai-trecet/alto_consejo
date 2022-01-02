37410aa5d8523f69a02b0fda0d3cf1e885d71d44
unai <unai13t@gmail.com>
2022-01-02 15:07:47 +0100

    Add nice seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,54 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'database_cleaner/active_record'
+require 'factory_bot_rails'
+
+DatabaseCleaner.clean_with(:truncation)
+
+def pick_random_object(model)
+  model.order(Arel.sql('RANDOM()')).first
+end
+
+puts 'Users creation!'
+
+100.times do |_i|
+  user = FactoryBot.create(:user, :confirmed)
+  print '.'
+end
+
+puts ''
+puts 'Games creation!'
+
+100.times do |_i|
+  game = FactoryBot.create(:game, user: pick_random_object(User))
+  print '.'
+end
+
+puts ''
+puts 'Matches creation!'
+
+500.times do |_i|
+  FactoryBot.create(:match, user: pick_random_object(User),
+                            game: pick_random_object(Game))
+  print '.'
+end
+
+puts ''
+puts 'MatchInvitations creation!'
+
+Match.all.each do |match|
+  MatchInvitationsManager.new(match: match).call
+  MatchParticipationManager.new(match_id: match.id, user_id: match.creator.id).call unless (match.id % 5).zero?
+  print '.'
+end
+
+puts ''
+puts 'MatchParticitations creation!'
+
+300.times do |_x|
+  match = Match.find_by(id: rand(500))
+  MatchParticipationManager.new(match_id: match.id, user_id: match.creator.id).call if match
+  print '.'
+end
+
+puts 'Seeding end!'
