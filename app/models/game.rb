8bd68dd21b71014e01b963a1e334e2b652acf262
unai <unai13t@gmail.com>
2022-01-03 11:14:09 +0100

    Some work on games index page.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,14 @@
 
 class Game < ApplicationRecord
   belongs_to :user
+  alias_attribute :added_by, :user
+
+  has_many :matches
 
   validates :user_id, :name, presence: true
   validates :name, uniqueness: true
+
+  def played_matches
+    matches.played
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,6 +4,19 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   it { should belong_to(:user) }
+  it { should have_many(:matches) }
+
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+
+  describe '#played_matches' do
+    subject { create(:game) }
+
+    it 'returns only passed matches' do
+      played_match = create(:match, game: subject, end_at: 1.hour.ago)
+      _future_match = create(:match, game: subject, end_at: 1.hour.since)
+
+      expect(subject.played_matches).to match_array([played_match])
+    end
+  end
 end
