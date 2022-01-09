355ce3fd35d5662bc3129742c76774505196c18d
unai <unai13t@gmail.com>
2022-01-09 21:45:53 +0100

    Test CalendarsController current action.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,6 +5,5 @@ class CalendarsController < ApplicationController
     start_date = params.fetch(:start_date, Date.today).to_date
     @matches = Match.related_to_user(current_user.id)
                     .where(start_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-                    .uniq
   end
 end
---
+++ spec/requests/calendars_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CalendarsController, type: :request do
+  let(:user) { create(:user, :confirmed) }
+  describe 'GET /matches_calendar' do
+    def call_action
+      get '/matches_calendar'
+    end
+
+    it_behaves_like 'not_logged_in'
+
+    it 'renders a successful response calling expeccted MAtch scope.' do
+      expect(Match).to receive(:related_to_user).with(user.id).and_call_original
+
+      sign_in(user)
+      call_action
+
+      expect(response).to be_successful
+      expect(response).to render_template('matches_calendar')
+    end
+  end
+end
