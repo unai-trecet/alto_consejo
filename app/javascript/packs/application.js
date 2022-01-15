3a926d37224ce6fbb61712cbd0b43489784c0471
unai <unai13t@gmail.com>
2022-01-15 11:37:29 +0100

    At last, Trix/Hotwire issue fixed :)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,41 @@
-require("@hotwired/turbo-rails")
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// 
+require.context('./images', true);
+
 import Rails from "@rails/ujs";
-require("@rails/activestorage").start()
+import "@hotwired/turbo-rails"
+import "trix"
+import "@rails/actiontext"
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "jquery";
+import "jquery-ui";
 
-window.Rails = Rails
+window.bootstrap = require("bootstrap");
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "@fortawesome/fontawesome-free/css/all";
 
-import 'bootstrap'
+import "./styles/application.scss";
 
+import('./admin');
+
+import './autocompleteUsername';
+import './autocompleteGameName';
+
+require("flatpickr")
+
+$("#match_start_at, #match_end_at").flatpickr({
+  enableTime: true,
+  dateFormat: "F, d Y H:i"
+});
+
+
+Rails.start()
+ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")
-import "controllers"
