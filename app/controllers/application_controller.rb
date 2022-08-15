4dabdafe1487f710fa0925ddb5f3b45824e6b468
Unai Trecet Acarreta <uani13t@gmail.com>
2022-08-15 12:13:37 +0200

    Fix logout dropdown menu and redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  private
+
   def record_not_found
     redirect_to root_path, notice: :not_found
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+   end
+
 end
