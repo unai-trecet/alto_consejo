1716e202f7b992ce46f3bd3d3064e69c30b5c7ee
Unai Trecet Acarreta <uani13t@gmail.com>
2022-08-16 18:53:48 +0200

    Tweak Games search with Stimulus controller delaying search request 700 msg.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,6 @@
 
 // require.context('./images', true);
 // import "./images"
-console.log('BRASAS 1');
-
 
 import Rails from "@rails/ujs";
 import "./jquery.js"
@@ -12,8 +10,6 @@ import "@rails/actiontext"
 import "trix"
 import * as ActiveStorage from "@rails/activestorage";
 import "./channels";
-
-console.log('BRASAS 2');
 
 import('./admin');
 
@@ -26,8 +22,6 @@ $("#match_start_at, #match_end_at").flatpickr({
   enableTime: true,
   dateFormat: "F, d Y H:i"
 });
-
-console.log('BRASAS 3');
 
 Rails.start()
 ActiveStorage.start()
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,22 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-// import { application } from "./application"
+import { application } from "./application"
 
-// import CommentReplyController from "./comment_reply_controller"
-// application.register("comment-reply", CommentReplyController)
-
-// import HelloController from "./hello_controller"
-// application.register("hello", HelloController)
-
-
-import { Application } from "@hotwired/stimulus"
-
-import HelloController from "./hello_controller"
 import CommentReplyController from "./comment_reply_controller"
+application.register("comment-reply", CommentReplyController)
 
-
-
-window.Stimulus = Application.start()
-Stimulus.register("hello", HelloController)
-Stimulus.register("comment-reply", CommentReplyController)
+import SearchFormController from "./search_form_controller"
+application.register("search-form", SearchFormController)
