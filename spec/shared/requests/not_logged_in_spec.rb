98e13dcba02ba9f61454e8335d2f7ae2cdbef8b7
Unai Trecet Acarreta <uani13t@gmail.com>
2023-11-10 13:15:56 +0100

    chore: add not_logged_in shared example

---
+++ spec/shared/requests/not_logged_in_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'not_logged_in' do
+  it 'redirects to login page' do
+    call_action
+
+    expect(response).to redirect_to(new_user_session_path)
+  end
+end
