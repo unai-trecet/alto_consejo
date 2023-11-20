dc176920455c1afec9f1e5795791c1f2715df8b7
Unai Trecet Acarreta <uani13t@gmail.com>
2023-11-20 20:30:06 +0100

    feat: show link to commentable page on comment view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_resource
   before_action :authenticate_user!
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
   def record_not_found
     redirect_to root_path, notice: :not_found
+  end
+
+  def set_resource # rubocop:disable Metrics/MethodLength
+    return unless params[:id]
+
+    @resource = case controller_name
+                when 'games'
+                  Game.find(params[:id])
+                when 'matches'
+                  Match.find(params[:id])
+                when 'users'
+                  User.find(params[:id])
+                when 'comments'
+                  Comment.find(params[:id])
+                end
   end
 end
