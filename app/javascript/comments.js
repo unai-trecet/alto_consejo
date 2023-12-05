d2701756a3db50a930a16d0abcbdcd92d6c8b928
Unai Trecet Acarreta <uani13t@gmail.com>
2023-12-05 17:05:35 +0100

    feat: hide comment action-buttons when the author is not the logged in user

--- app/javascript/comments.js
+++ app/javascript/comments.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+function updateComments() {
   const userId = document.body.dataset.currentUserId;
   const comments = document.querySelectorAll('[data-user-id]');
-  console.log(userId);
 
   comments.forEach((comment) => {
-    console.log(comment.dataset.userId);
     if (comment.dataset.userId === userId) {
-      comment.querySelector('.comment-actions').classList.remove('d-none');
+      let commentActions = comment.querySelector('.comment-actions');
+      commentActions.classList.remove('d-none');
     }
+
+    // const heartIcon = comment.querySelector('.bi-heart');
+    // if (comment.dataset.voted === 'true') {
+    //   console.log('voted');
+    //   heartIcon.classList.remove('bi-heart');
+    //   heartIcon.classList.add('bi-heart-fill', 'text-danger');
+    // } else {
+    //   console.log('not voted');
+    //   heartIcon.classList.remove('bi-heart-fill', 'text-danger');
+    //   heartIcon.classList.add('bi-heart');
+    // }
   });
-});
+}
+
+document.addEventListener('DOMContentLoaded', updateComments);
+document.addEventListener('turbolinks:load', updateComments);
