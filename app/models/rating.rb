13f0128e724d0ee78dd4db289c52935fbb54c70d
Unai Trecet Acarreta <uani13t@gmail.com>
2023-12-11 17:39:37 +0100

    feat: make rating from 0 to 10

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,6 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :rateable, polymorphic: true
 
-  validates :value, presence: true, inclusion: { in: 0..5 }
+  validates :value, presence: true, inclusion: { in: 0..10 }
   validates :user_id, uniqueness: { scope: %i[rateable_type rateable_id], message: 'can rate only once per item' }
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe Rating, type: :model do
   it { should belong_to(:rateable) }
 
   it { should validate_presence_of(:value) }
-  it { should validate_inclusion_of(:value).in_range(0..5) }
+  it { should validate_inclusion_of(:value).in_range(0..10) }
 
   describe 'uniqueness validation' do
     subject { Rating.new(user: create(:user), rateable: create(:game), value: 3) }
-    it { should validate_uniqueness_of(:user_id).scoped_to(:rateable_type, :rateable_id).with_message('can rate only once per item') }
+    it {
+      should validate_uniqueness_of(:user_id).scoped_to(:rateable_type,
+                                                        :rateable_id).with_message('can rate only once per item')
+    }
   end
 end
