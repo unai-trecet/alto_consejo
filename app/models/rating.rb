08d11058dbe8291d07cc54a906624906fa561f6e
Unai Trecet Acarreta <uani13t@gmail.com>
2023-12-12 12:40:35 +0100

    feat: broadcast average_rating after new rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,17 @@
 class Rating < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :user
   belongs_to :rateable, polymorphic: true
 
   validates :value, presence: true, inclusion: { in: 0..10 }
   validates :user_id, uniqueness: { scope: %i[rateable_type rateable_id], message: 'can rate only once per item' }
+
+  after_commit :broadcast_rating
+  def broadcast_rating
+    broadcast_replace_to [rateable, :average_rating],
+                         target: dom_id(rateable, :average_rating),
+                         partial: 'ratings/average_rating_stars',
+                         locals: { rateable: rateable }
+  end
 end
