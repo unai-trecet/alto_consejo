deb30969b190180309578307edc303edc334166e
Unai Trecet Acarreta <uani13t@gmail.com>
2023-12-29 11:40:24 +0100

    feat: refactor stimulus charts_controller and view

--- app/javascript/controllers/charts_controller.js
+++ app/javascript/controllers/charts_controller.js
@@ -4,19 +4,28 @@ import { Controller } from "@hotwired/stimulus"
 import { Chart } from 'chart.js';
 
 export default class extends Controller {
-  playedMatchesChartInstance = null;
-  organizedMatchesChartInstance = null;
+  chartInstances = {};
 
   connect() {
-    // console.log("Charts controller connected!")
-    this.updateUserPlayedMatches();
-    this.updateUserOrganizedMatches();
+    console.log("Charts controller connected!")
+    this.updateChart(null, 'user_played_matches', 'user_played_matches_chart');
+    this.updateChart(null, 'user_organized_matches', 'user_organized_matches_chart');
   }
 
-  updateUserPlayedMatches(event) {
-    const selectedValue = event ? event.target.value : 'default_value';
+  updateChart(event, defaultEndpoint, defaultCanvasId) {
+    let selectedValue, endpoint, canvasId;
   
-    fetch(`/graphs/user_played_matches?from_date=${selectedValue}`, {
+    if (event) {
+      selectedValue = event.target.value;
+      endpoint = event.target.dataset.chartsEndpoint;
+      canvasId = event.target.dataset.chartsCanvasId;
+    } else {
+      selectedValue = 'default_value';
+      endpoint = defaultEndpoint;
+      canvasId = defaultCanvasId;
+    }
+  
+    fetch(`/graphs/${endpoint}?from_date=${selectedValue}`, {
       headers: { accept: "application/json" }
     })
     .then(response => {
@@ -41,71 +50,15 @@ export default class extends Controller {
       };
   
       // Get the canvas from the DOM
-      const canvas = document.getElementById('user_played_matches_chart');
+      const canvas = document.getElementById(canvasId);
 
       // Destroy the existing chart if there is one
-      if (this.playedMatchesChartInstance) {
-        this.playedMatchesChartInstance.destroy();
+      if (this.chartInstances[canvasId]) {
+        this.chartInstances[canvasId].destroy();
       }
   
       // Create a new chart on the canvas
-      this.playedMatchesChartInstance = new Chart(canvas, {
-        type: 'line',
-        data: transformedData,
-        options: {
-          scales: {
-            y: {
-              ticks: {
-                callback: function(value, index, values) {
-                  return Number.isInteger(value) ? value : null;
-                }
-              }
-            }
-          }
-        } // add your chart options here
-      });
-    })
-    .catch(error => console.log('There was an error:', error));
-  }
-
-  updateUserOrganizedMatches(event) {
-    console.log("updateUserOrganizedMatches");
-    const selectedValue = event ? event.target.value : 'default_value';
-  
-    fetch(`/graphs/user_organized_matches?from_date=${selectedValue}`, {
-      headers: { accept: "application/json" }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      // Transform the data into the format expected by Chart.js
-      const labels = Object.keys(data);
-      const datasetData = Object.values(data);
-      const transformedData = {
-        labels: labels,
-        datasets: [{
-          label: 'Played Matches',
-          data: datasetData,
-          fill: false,
-          borderColor: 'rgb(0, 0, 255)',
-          tension: 0.1
-        }]
-      };
-  
-      // Get the canvas from the DOM
-      const canvas = document.getElementById('user_organized_matches_chart');
-
-      // Destroy the existing chart if there is one
-      if (this.organizedMatchesChartInstance) {
-        this.organizedMatchesChartInstance.destroy();
-      }
-  
-      // Create a new chart on the canvas
-      this.organizedMatchesChartInstance = new Chart(canvas, {
+      this.chartInstances[canvasId] = new Chart(canvas, {
         type: 'line',
         data: transformedData,
         options: {
