35c63eee2e6d5c902853143843f20e7766f5b7e5
Unai Trecet Acarreta <uani13t@gmail.com>
2024-01-09 13:12:53 +0100

    test: use better unique game name

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
-
 FactoryBot.define do
   factory :game do
-    name { Faker::Game.unique.title }
+    name { Faker::Game.title + Faker::Number.number(digits: 10).to_s }
     description { Faker::Quotes::Chiquito.sentence }
     author { Faker::Book.author }
     user { create(:user, :confirmed) }
     bbg_link { "www.board_game_geek.com/#{Faker::Number.number(digits: 10)}" }
-    image { 'an image' }
   end
 end
